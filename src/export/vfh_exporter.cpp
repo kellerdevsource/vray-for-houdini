6754d6150600db7546e40cdf07dd8ab3e7b107d4
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2015-10-19 14:25:06 +0300

    fix: move view into a separate file

--- src/export/vfh_exporter.cpp
+++ src/export/vfh_exporter.cpp
@@ -370,42 +370,6 @@ void VRayExporter::RtCallbackView(OP_Node *caller, void *callee, OP_EventType ty
 }
 
 
-void VRayExporter::exportCamera(OP_Node *camera)
-{
-	addOpCallback(camera, VRayExporter::RtCallbackView);
-
-	OBJ_Node *obj_camera = camera->castToOBJNode();
-
-	// https://www.sidefx.com/docs/houdini13.0/ref/cameralenses
-	//   fovx = 2 * atn( (apx/2) / focal )
-	//
-	const float apx   = camera->evalFloat("aperture", 0, m_context.getTime());
-	const float focal = camera->evalFloat("focal", 0, m_context.getTime());
-
-	const float fovx  = 2.0f * atanf((apx / 2.0f) / focal);
-
-	Attrs::PluginDesc renderView("cameraView", "RenderView");
-	renderView.addAttribute(Attrs::PluginAttr("transform", VRayExporter::GetOBJTransform(obj_camera, m_context)));
-	renderView.addAttribute(Attrs::PluginAttr("fov", fovx));
-
-	m_renderer.exportPlugin(renderView);
-}
-
-
-int VRayExporter::exportView(OP_Node *rop)
-{
-	OP_Node *camera = VRayExporter::GetCamera(rop);
-	if (!camera) {
-		PRINT_ERROR("Camera is not set!");
-	}
-	else {
-		exportCamera(camera);
-	}
-
-	return 0;
-}
-
-
 int VRayExporter::exportSettings(OP_Node *rop)
 {
 	for (const auto &sp : Parm::RenderSettingsPlugins) {
