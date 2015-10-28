6cc5806d00a62ca4c64a28c441269a487f9fe688
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2015-10-28 20:26:26 +0200

    fix: no need for name

--- cmake/vfh_compiler.cmake
+++ cmake/vfh_compiler.cmake
@@ -3,8 +3,6 @@
 #
 # V-Ray For Houdini
 #
-# Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
-#
 # ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
 #
 # Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
