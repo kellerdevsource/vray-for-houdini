6da520bb796e955a1e3e4016330b759374d38bf7
Mila Grigorova <mila.grigorova@chaosgroup.com>
2015-11-23 18:09:34 +0200

    fix dome light viewport geometry

--- deploy/scripts/nodeinit/obj/VRayNodeLightDome.py
+++ deploy/scripts/nodeinit/obj/VRayNodeLightDome.py
@@ -12,21 +12,61 @@ import hou
 # The node is passed in kwargs['node']
 node = kwargs['node']
 
-fnode = node.createNode("file")
-node_parm = fnode.parm("file")
+cnode = node.createNode("file")
+node_parm = cnode.parm("file")
 node_parm.set("vfhlightdome.bgeo")
-node_parm = fnode.parm("loadtype")
-node_parm.set('delayed')
-node_parm = fnode.parm("viewportlod")
-node_parm.set('full')
 
-tnode = fnode.createOutputNode("xform")
-node_parm = tnode.parm("scale");
+cnode = cnode.createOutputNode("xform")
+node_parm = cnode.parm("scale");
 node_parm.setExpression("ch(\"../dome_targetRadius\")")
 
-rnode = tnode.createOutputNode("null")
-rnode.setName("OUT")
-rnode.setRenderFlag(True)
-rnode.setDisplayFlag(True)
+cnode = cnode.createOutputNode("xform")
+node_parm = cnode.parm("group")
+node_parm.set("gcompass")
+node_parm = cnode.parm("rOrd")
+node_parm.setExpression("ch(\"../rOrd\")")
+node_parm = cnode.parm("rx");
+node_parm.setExpression("ch(\"../rx\")")
+node_parm = cnode.parm("ry");
+node_parm.setExpression("ch(\"../ry\")")
+node_parm = cnode.parm("rz");
+node_parm.setExpression("ch(\"../rz\")")
+
+cnode = cnode.createOutputNode("xform")
+node_parm = cnode.parm("xOrd")
+node_parm.setExpression("ch(\"../xOrd\")")
+node_parm = cnode.parm("rOrd")
+node_parm.setExpression("ch(\"../rOrd\")")
+node_parm = cnode.parm("rx");
+node_parm.setExpression("ch(\"../rx\")")
+node_parm = cnode.parm("ry");
+node_parm.setExpression("ch(\"../ry\")")
+node_parm = cnode.parm("rz");
+node_parm.setExpression("ch(\"../rz\")")
+node_parm = cnode.parm("tx");
+node_parm.setExpression("ch(\"../tx\")")
+node_parm = cnode.parm("ty");
+node_parm.setExpression("ch(\"../ty\")")
+node_parm = cnode.parm("tz");
+node_parm.setExpression("ch(\"../tz\")")
+node_parm = cnode.parm("px");
+node_parm.setExpression("ch(\"../px\")")
+node_parm = cnode.parm("py");
+node_parm.setExpression("ch(\"../py\")")
+node_parm = cnode.parm("pz");
+node_parm.setExpression("ch(\"../pz\")")
+node_parm = cnode.parm("invertxform")
+node_parm.set(True)
+
+cnode = cnode.createOutputNode("delete")
+node_parm = cnode.parm("negate");
+node_parm.set("keep")
+node_parm = cnode.parm("group");
+node_parm.setExpression("ifs(ch(\"../dome_spherical\"), \"gsphere\",\"ghemisphere\")")
+
+cnode = cnode.createOutputNode("null")
+cnode.setName("OUT")
+cnode.setRenderFlag(True)
+cnode.setDisplayFlag(True)
 
 node.layoutChildren()
