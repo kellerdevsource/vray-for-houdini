dc090ff3729b795143a51d41a937f27070255fad
Mila Grigorova <mila.grigorova@chaosgroup.com>
2015-11-30 18:15:25 +0200

    minor adjustments

--- src/vfh_register.cpp
+++ src/vfh_register.cpp
@@ -92,16 +92,16 @@ void newSopOperator(OP_OperatorTable *table)
 
 void newObjectOperator(OP_OperatorTable *table)
 {
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", SunLight);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightDirect);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightAmbient);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightOmni);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightSphere);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightSpot);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightRectangle);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightMesh);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightIES);
-	VFH_OBJ_ADD_OPERATOR_AUTO(table, "LIGHT", LightDome);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, SunLight);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightDirect);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightAmbient);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightOmni);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightSphere);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightSpot);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightRectangle);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightMesh);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightIES);
+	VFH_OBJ_ADD_OPERATOR_AUTO(table, OBJ::VRayPluginType::Light, LightDome);
 }
 
 
