9d9e6073c9328cdabeb175a1d4fd8fcb00b61a4e
Mila Grigorova <mila.grigorova@chaosgroup.com>
2015-11-30 18:14:30 +0200

    adjusting vfh_shelf.py to generate object shelf tools in vfh_obj.shelf

--- deploy/tools/vfh_shelf.py
+++ deploy/tools/vfh_shelf.py
@@ -4,60 +4,115 @@ import os
 
 SHELF_TMPL = open(os.path.join(os.path.dirname(__file__), "vfh_vop.shelf.in"), 'r').read()
 TOOL_TMPL  = open(os.path.join(os.path.dirname(__file__), "vfh_tool.in"), 'r').read()
+PLUGINS_DESCS = {}
 
 descDirpath = pathlib.Path(os.path.join(os.path.dirname(__file__), "..", "plugins_desc"))
 
-PLUGINS_DESCS = {}
 
-for filePath in descDirpath.glob("*/*.json"):
-    pluginDesc = json.loads(filePath.open().read())
+def getPluginsDescs():
+    if len(PLUGINS_DESCS) != 0:
+        return PLUGINS_DESCS
 
-    pluginID     = pluginDesc.get('ID')
-    pluginParams = pluginDesc.get('Parameters')
-    pluginName   = pluginDesc.get('Name')
-    pluginType   = pluginDesc.get('Type')
-    plugiIDDesc  = pluginDesc.get('Description', "")
-    plguinWidget = pluginDesc.get('Widget', {})
+    for filePath in descDirpath.glob("*/*.json"):
+        pluginDesc = json.loads(filePath.open().read())
 
-    PLUGINS_DESCS[pluginID] = {
-        'ID'         : pluginID,
-        'Name'       : pluginName,
-        'Type'       : pluginType,
-        'Parameters' : pluginParams,
-        'Widget'     : plguinWidget,
-    }
+        pluginID     = pluginDesc.get('ID')
+        pluginParams = pluginDesc.get('Parameters')
+        pluginName   = pluginDesc.get('Name')
+        pluginType   = pluginDesc.get('Type')
+        plugiIDDesc  = pluginDesc.get('Description', "")
+        plguinWidget = pluginDesc.get('Widget', {})
 
-tools = ""
+        PLUGINS_DESCS[pluginID] = {
+            'ID'         : pluginID,
+            'Name'       : pluginName,
+            'Type'       : pluginType,
+            'Parameters' : pluginParams,
+            'Widget'     : plguinWidget,
+        }
 
-for pluginID in sorted(PLUGINS_DESCS):
-    pluginDesc = PLUGINS_DESCS[pluginID]
+    return PLUGINS_DESCS
 
-    name = pluginDesc['Name']
-    node_name = name.replace(" ", "").lower()
-    for c in ";:@|(":
-        node_name = node_name.replace(c, "_")
-    node_name = node_name.replace(")", "")
 
-    menu_name = pluginDesc['Type']
+def isInContextOBJ(pluginID):
+    pluginsDescs = getPluginsDescs()
+    pluginDesc = pluginsDescs[pluginID]
+    pluginType = pluginDesc['Type']
+    if pluginType == "LIGHT":
+        return True
 
-    if menu_name == "RENDERCHANNEL":
-        menu_name = "Render Channel"
-    elif menu_name == "UVWGEN":
-        menu_name = "Mapping"
+    return False
 
-    if menu_name != "BRDF":
-        menu_name = menu_name.title()
 
-    tools += TOOL_TMPL.format(
-        CONTEXT="vop",
-        OP_CONTEXT="Vop",
-        OP_NAME="VRayNode%s" % pluginDesc['ID'],
-        OP_NODE_NAME=node_name,
-        ITEM_NAME=name,
-        ITEM_MENU=menu_name,
-    )
-    tools += "\n"
+def genShelf_vfh_vop(shelfPath):
+    tools = ""
+    pluginsDescs = getPluginsDescs()
+    for pluginID in sorted(pluginsDescs):
+        pluginDesc = pluginsDescs[pluginID]
 
-shelf = SHELF_TMPL.format(TOOLS=tools)
+        name = pluginDesc['Name']
+        node_name = name.replace(" ", "").lower()
+        for c in ";:@|(":
+            node_name = node_name.replace(c, "_")
+        node_name = node_name.replace(")", "")
 
-print(shelf)
+        menu_name = pluginDesc['Type']
+
+        if menu_name == "RENDERCHANNEL":
+            menu_name = "Render Channel"
+        elif menu_name == "UVWGEN":
+            menu_name = "Mapping"
+
+        if menu_name != "BRDF":
+            menu_name = menu_name.title()
+
+        tools += TOOL_TMPL.format(
+            CONTEXT="vop",
+            NET_CONTEXT="VOP",
+            OP_CONTEXT="Vop",
+            OP_NAME="VRayNode%s" % pluginDesc['ID'],
+            OP_NODE_NAME=node_name,
+            ITEM_NAME=name,
+            ITEM_MENU=menu_name,
+        )
+        tools += "\n"
+
+    shelf = SHELF_TMPL.format(TOOLS=tools)
+    with open(shelfPath, "w") as f:
+        f.write(shelf)
+
+
+def genShelf_vfh_obj(shelfPath):
+    tools = ""
+    pluginsDescs = getPluginsDescs()
+    for pluginID in sorted(pluginsDescs):
+        if isInContextOBJ(pluginID):
+            pluginDesc = pluginsDescs[pluginID]
+            menu_name = "V-Ray"
+
+            tools += TOOL_TMPL.format(
+                CONTEXT="object",
+                NET_CONTEXT="OBJ",
+                OP_CONTEXT="Object",
+                OP_NAME="VRayNode%s" % pluginID,
+                OP_NODE_NAME=pluginID.lower(),
+                ITEM_NAME=pluginID,
+                ITEM_MENU=menu_name,
+            )
+            tools += "\n"
+
+    shelf = SHELF_TMPL.format(TOOLS=tools)
+    with open(shelfPath, "w") as f:
+        f.write(shelf)
+
+
+def main():
+    getPluginsDescs()
+    vopShelfPath = os.path.join(os.path.dirname(__file__), "..", "vfh_vop.shelf")
+    genShelf_vfh_vop(vopShelfPath)
+    objShelfPath = os.path.join(os.path.dirname(__file__), "..", "vfh_obj.shelf")
+    genShelf_vfh_obj(objShelfPath)
+
+
+if __name__ == "__main__":
+    main()
