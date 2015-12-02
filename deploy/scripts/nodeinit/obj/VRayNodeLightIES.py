13cd962e64bfdb37912a474e1329ff2ae5c91d7e
Mila Grigorova <mila.grigorova@chaosgroup.com>
2015-12-02 14:38:12 +0200

    add geometry for LightIES

---
+++ deploy/scripts/nodeinit/obj/VRayNodeLightIES.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2015, Chaos Software Ltd
+#
+# V-Ray For Houdini
+#
+# ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+#
+# Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+#
+
+import hou
+
+# The node is passed in kwargs['node']
+node = kwargs['node']
+
+boxnode = node.createNode("box")
+
+circlenode = node.createNode("circle")
+node_parm = circlenode.parm("orient")
+node_parm.set("xy")
+node_parm = circlenode.parm("tz")
+node_parm.setExpression("-0.5 * ch(\"../%s/scale\")" % boxnode.name())
+node_parm = circlenode.parm("scale")
+node_parm.setExpression("0.4 * ch(\"../%s/scale\")" % boxnode.name())
+
+mergenode = node.createNode("merge")
+mergenode.setInput(0, boxnode, 0)
+mergenode.setInput(1, circlenode, 0)
+
+mergenode.setRenderFlag(True)
+mergenode.setDisplayFlag(True)
+
+node.layoutChildren()
