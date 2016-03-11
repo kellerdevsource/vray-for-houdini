5b3d67c6fa1fc502f7c0be5e4f52b23f161b867b
Jefferson Lim <galagast@gmail.com>
2016-03-11 23:28:43 +0800

    updated methods called by the render shelf tools
    * It will now render the selected V-Ray ROP.
    * If no V-Ray ROP is selected, it will use the last selected V-Ray ROP.

--- deploy/scripts/python/vfh/vfh_rop.py
+++ deploy/scripts/python/vfh/vfh_rop.py
@@ -11,20 +11,37 @@
 import hou
 
 
-def _getCreateRop():
-    vrayNode = hou.node("/out/vray_renderer1")
-
-    if not vrayNode:
-        vrayNode = hou.node("/out").createNode("vray_renderer")
-
+def _createVRayRop():
+    vrayNode = hou.node("/out").createNode("vray_renderer")
+    
     return vrayNode
+        
+def _getVRayRop():
+    vrayROP = hou.node(hou.getenv("curVRayROP"))
+    
+    vray_node_types = hou.nodeType(hou.ropNodeTypeCategory(), "vray_renderer")
+    vray_nodes = vray_node_types.instances()
+    sel_vray_nodes = [ i for i in vray_nodes if i.isSelected() ]
+    
+    if len(sel_vray_nodes) > 0:
+        vrayROP = sel_vray_nodes[0]
+        hou.putenv("curVRayROP", vrayROP.path())
+    else:
+        if not vrayROP:
+            vrayROP = _createVRayRop()
+            hou.putenv("curVRayROP", vrayROP.path())
+        else:
+            vrayROP = hou.node(hou.getenv("curVRayROP"))
+    print "V-Ray Renderer Node: " + vrayROP.name()
+	
+    return vrayROP
 
 
 def render():
-    vrayNode = _getCreateRop()
+    vrayNode = _getVRayRop()
     vrayNode.parm('execute').pressButton()
 
 
 def render_rt():
-    vrayNode = _getCreateRop()
+    vrayNode = _getVRayRop()
     vrayNode.parm('render_rt').pressButton()
