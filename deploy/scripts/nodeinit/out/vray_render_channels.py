c5b0f4315f30497954ecad39e9ccbf0ed3eb1852
Mila Grigorova <mila.grigorova@chaosgroup.com>
2016-04-25 17:31:33 +0300

    fix #64: add creation script for render_channels network node

---
+++ deploy/scripts/nodeinit/out/vray_render_channels.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2015-2016, Chaos Software Ltd
+#
+# V-Ray For Houdini
+#
+# ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+#
+# Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+#
+
+import hou
+
+# The node is passed in kwargs['node']
+node = kwargs['node']
+cnode = node.createNode("VRayNodeRenderChannelsContainer")
+
+node.layoutChildren()
