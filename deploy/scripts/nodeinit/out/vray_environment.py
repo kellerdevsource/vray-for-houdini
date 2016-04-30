9f2ab140c1d9567ac5192be60253044928a0fd9b
Jefferson Lim <galagast@gmail.com>
2016-04-30 21:40:12 +0800

    #64 add creation script for render environment network node
    creates an "environment settings" node and connects a typical image
    file/texture setup with mapping set to spherical environment.

---
+++ deploy/scripts/nodeinit/out/vray_environment.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2015-2016, Chaos Software Ltd
+#
+# V-Ray For Houdini
+#
+# ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+#
+# Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+#
+
+import hou
+
+# The node is passed in kwargs['node']
+node = kwargs['node']
+
+envNode = node.createNode("VRayNodeSettingsEnvironment")
+
+imageTexNode = envNode.createInputNode(0, "VRayNodeTexBitmap")	# bg_tex
+envNode.setInput(1, imageTexNode, 0)							# gi_tex
+envNode.setInput(2, imageTexNode, 0)							# reflect_tex
+envNode.setInput(3, imageTexNode, 0)							# refract_tex
+
+uvwGenEnvNode = imageTexNode.createInputNode(5, "VRayNodeUVWGenEnvironment")
+imageFileNode = imageTexNode.createInputNode(6, "VRayNodeBitmapBuffer")
+
+envNode.setName("environment_settings")
+uvwGenEnvNode.setName("environment_mapping")
+imageTexNode.setName("imagetexture")
+imageFileNode.setName("imagefile")
+
+node.layoutChildren()
