1d59fc89e7ba6d999ca3d348386567f1d226444f
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-10-18 12:12:52 +0300

    fix: actually use default compiler on linux

--- deploy/ci/jenkins.py
+++ deploy/ci/jenkins.py
@@ -182,4 +182,8 @@ if __name__ == '__main__':
             sys.exit(-1)
 
     args = ArgsReplacement()
+    if TC.getPlatformSuffix() == 'linux':
+        args.CGR_CXX_COMPILER = 'g++'
+        args.CGR_C_COMPILER = 'gcc'
+
     TC.main(args)
