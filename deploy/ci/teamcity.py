7996cff6f9c36b0e3604ee31e434fb57c5260346
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2016-10-19 13:58:06 +0300

    Revert "HOU-5 Implement Phoenix diagrams"
    This reverts commit 672693ba2df1bde745369759e1a65523bfca4f4a, reversing
    changes made to eba3d69a273e294e5be23e815397c63f37ec638c.

--- deploy/ci/teamcity.py
+++ deploy/ci/teamcity.py
@@ -18,22 +18,14 @@ import tempfile
 
 
 # Build mode: 'nightly', 'stable', 'debug'
-_cgr_build_mode = None
-_cgr_build_type = None
-_cgr_release_root = None
-_cgr_config_root = None
+_cgr_build_mode = os.environ['CGR_BUILD_MODE']
+_cgr_build_type = os.environ['CGR_BUILD_TYPE']
+_cgr_release_root = os.environ['CGR_RELEASE_ROOT']
+_cgr_config_root = os.environ['CGR_CONFIG_ROOT']
 
 
 def toCmakePath(path):
     return os.path.normpath(path).replace("\\", "/")
-
-def getPlatformSuffix():
-    if sys.platform == 'win32':
-        return "windows"
-    elif sys.platform == 'linux':
-        return "linux"
-    return "osx"
-
 
 
 def cleanDir(dirpath):
@@ -118,24 +110,18 @@ def upload(filepath):
 
 # Setup Visual Studio variables for command line usage
 #
-def set_utils_paths():
-    if getPlatformSuffix() != 'windows':
-        return
-
-    env = {
-        'PATH': [
-            "C:/Program Files (x86)/CMake/bin",
-            "C:/Program Files/SlikSvn/bin",
-            "C:/Program Files/Git/bin",
-            "C:/Program Files (x86)/WinSCP",
-        ] + os.environ['PATH'].split(os.pathsep)
-    }
-
-    for var in env:
-        os.environ[var] = os.pathsep.join(env[var])
+def get_utils_paths():
+    return [
+        "C:/Program Files (x86)/CMake/bin",
+        "C:/Program Files/SlikSvn/bin",
+        "C:/Program Files/Git/bin",
+        "C:/Program Files (x86)/WinSCP",
+    ]
 
 
-def setup_msvc_2012(cgrSdkPath):
+def setup_msvc_2012():
+    cgrSdkPath = os.environ['CGR_SDK']
+
     env = {
         'INCLUDE' : [
             "{CGR_SDK}/msvs2012/PlatformSDK/Include/shared",
@@ -151,7 +137,8 @@ def setup_msvc_2012(cgrSdkPath):
             "{CGR_SDK}/msvs2012/lib/amd64",
         ],
 
-        'PATH' : os.environ['PATH'].split(os.pathsep) +
+        'PATH' : os.environ['PATH'].split(';') + 
+            get_utils_paths() +
             [
                 "{CGR_SDK}/msvs2012/bin/amd64",
                 "{CGR_SDK}/msvs2012/bin",
@@ -161,10 +148,12 @@ def setup_msvc_2012(cgrSdkPath):
     }
 
     for var in env:
-        os.environ[var] = os.pathsep.join(env[var]).format(CGR_SDK=cgrSdkPath)
+        os.environ[var] = ";".join(env[var]).format(CGR_SDK=cgrSdkPath)
 
 
-def setup_msvc_2015(cgrSdkPath):
+def setup_msvc_2015():
+    cgrSdkPath = os.environ['CGR_SDK']
+
     env = {
         'INCLUDE' : [
             "{CGR_SDK}/msvs2015/PlatformSDK/Include/shared",
@@ -182,7 +171,8 @@ def setup_msvc_2015(cgrSdkPath):
             "{CGR_SDK}/msvs2015/lib/amd64",
         ],
 
-        'PATH' : os.environ['PATH'].split(os.pathsep) +
+        'PATH' : os.environ['PATH'].split(';') +
+            get_utils_paths() +
             [
                 "{CGR_SDK}/msvs2015/bin/amd64",
                 "{CGR_SDK}/msvs2015/bin",
@@ -193,7 +183,7 @@ def setup_msvc_2015(cgrSdkPath):
 
     os.environ['__MS_VC_INSTALL_PATH'] = "{CGR_SDK}/msvs2015"
     for var in env:
-        os.environ[var] = os.pathsep.join(env[var]).format(CGR_SDK=cgrSdkPath)
+        os.environ[var] = ";".join(env[var]).format(CGR_SDK=cgrSdkPath)
 
 
 def getArchiveExt():
@@ -202,92 +192,53 @@ def getArchiveExt():
     return "tar.bz2"
 
 
-def which(program, add_ext=False):
-    """
-      Returns full path of "program" or None
-    """
-
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            sys.stdout.write('Checking if path [%s] is path for [%s] ... ' % (exe_file, fname))
-            if is_exe(exe_file):
-                sys.stdout.write('yes!\n')
-                sys.stdout.flush()
-                return exe_file
-            sys.stdout.write('no!\n')
-            sys.stdout.flush()
-
-        if getPlatformSuffix() == 'windows' and not add_ext:
-            return which('%s.exe' % program, True)
-
-    return None
+def getPlatformSuffix():
+    if sys.platform == 'win32':
+        return "windows"
+    elif sys.platform == 'linux':
+        return "linux"
+    return "osx"
 
 
 def main(args):
-    # lets polute global space
-    global _cgr_build_mode
-    global _cgr_build_type
-    global _cgr_release_root
-    global _cgr_config_root
-
-    _cgr_build_mode = args.CGR_BUILD_MODE
-    _cgr_build_type = args.CGR_BUILD_TYPE
-    _cgr_release_root = args.CGR_RELEASE_ROOT
-    _cgr_config_root = args.CGR_CONFIG_ROOT
-
-    set_utils_paths()
     srcHash = args.src_hash[:7]
 
     ReleaseDir = os.path.expanduser("%s/vray_for_houdini/%s" % (_cgr_release_root, sys.platform))
     ReleaseArchive = os.path.join(ReleaseDir, "vfh-{BUILD_NUMBER}-{SRC_GIT_HASH}-hfs{HOUDINI_VERSION}.{HOUDINI_VERSION_BUILD}-{OS}{DEBUG}.{EXT}".format(
         SRC_GIT_HASH=srcHash,
-        HOUDINI_VERSION=args.CGR_HOUDINI_VERSION,
-        HOUDINI_VERSION_BUILD=args.CGR_HOUDINI_VERSION_BUILD,
+        HOUDINI_VERSION=os.environ['CGR_HOUDINI_VERSION'],
+        HOUDINI_VERSION_BUILD=os.environ['CGR_HOUDINI_VERSION_BUILD'],
         EXT=getArchiveExt(),
         OS=getPlatformSuffix(),
-        BUILD_NUMBER=args.BUILD_NUMBER,
+        BUILD_NUMBER=os.environ['BUILD_NUMBER'],
         DEBUG="-dbg" if _cgr_build_type == "Debug" else "",
     ))
-    SdkPath = args.CGR_SDKPATH
+    SdkPath = os.environ.get('CGR_SDKPATH', "")
 
-    ninja_path = os.path.dirname(which('ninja'))
-
-    cmake = [which('cmake')]
+    cmake = ["cmake"]
     cmake.append('-GNinja')
 
-    old_path = os.environ['PATH']
-    if args.jenkins and getPlatformSuffix() == 'windows':
-        # clear all possible conflicts from path
-        os.environ['PATH'] = os.pathsep.join([ninja_path, os.path.dirname(which('git')), os.path.dirname(which('7z'))])
-
-    if sys.platform == 'win32':
-        houdiniMajorVer = float(args.CGR_HOUDINI_VERSION)
-        if houdiniMajorVer >= 15.5:
-            cmake.append('-DMSVC_VERSION=1900')
-            setup_msvc_2015(args.CGR_SDK)
-        else:
-            cmake.append('-DMSVC_VERSION=1800')
-            setup_msvc_2012(args.CGR_SDK)
-
     if sys.platform == 'linux':
-        cmake.append('-DCMAKE_CXX_COMPILER=%s' % args.CGR_CXX_COMPILER)
+        cmake.append('-DCMAKE_CXX_COMPILER=%s' % os.environ.get('CGR_CXX_COMPILER', "/usr/bin/g++4.8"))
         cmake.append('-DWITH_STATIC_LIBC=ON')
 
     cmake.append('-DCMAKE_BUILD_TYPE=%s' % _cgr_build_type)
     cmake.append('-DSDK_PATH=%s'              % toCmakePath(SdkPath))
-    cmake.append('-DHOUDINI_VERSION=%s'       % args.CGR_HOUDINI_VERSION)
-    cmake.append('-DHOUDINI_VERSION_BUILD=%s' % args.CGR_HOUDINI_VERSION_BUILD)
-    cmake.append('-DAPPSDK_VERSION=%s'        % args.CGR_APPSDK_VERSION)
-    cmake.append('-DPHXSDK_VERSION=%s'        % args.CGR_PHXSDK_VERSION)
-    cmake.append('-DVRAYSDK_VERSION=%s'       % args.CGR_VRAYSDK_VERSION)
+    cmake.append('-DHOUDINI_VERSION=%s'       % os.environ['CGR_HOUDINI_VERSION'])
+    cmake.append('-DHOUDINI_VERSION_BUILD=%s' % os.environ['CGR_HOUDINI_VERSION_BUILD'])
+    cmake.append('-DAPPSDK_VERSION=%s'        % os.environ['CGR_APPSDK_VERSION'])
+    cmake.append('-DPHXSDK_VERSION=%s'        % os.environ['CGR_PHXSDK_VERSION'])
+    cmake.append('-DVRAYSDK_VERSION=%s'       % os.environ['CGR_VRAYSDK_VERSION'])
+
+    if sys.platform == 'win32':
+        houdiniMajorVer = float(os.environ['CGR_HOUDINI_VERSION'])
+        if houdiniMajorVer >= 15.5:
+            cmake.append('-DMSVC_VERSION=1900')
+            setup_msvc_2015()
+        else:
+            cmake.append('-DMSVC_VERSION=1800')
+            setup_msvc_2012()
+
     cmake.append('-DCGR_SRC_HASH=%s' % srcHash)
     cmake.append('-DUSE_LAUNCHER=OFF')
     cmake.append('-DINSTALL_LOCAL=OFF')
@@ -305,67 +256,26 @@ def main(args):
         # clean build dir
         cleanDir(os.getcwd())
 
-    sys.stdout.write('PATH:\n\t%s\n' % '\n\t'.join(os.environ['PATH'].split(os.pathsep)))
-    sys.stdout.write('cmake args:\n%s\n' % '\n\t'.join(cmake))
-    sys.stdout.flush()
-
     err = call(cmake)
 
     if not err:
         err = call(ninja)
 
-    os.environ['PATH'] = old_path
     if not err:
         if args.upload:
             upload(ReleaseArchive)
 
-        sys.stdout.write("##teamcity[setParameter name='env.CGR_ARTIFACTS' value='%s']" % (ReleaseArchive))
-        sys.stdout.flush()
+    sys.stdout.write("##teamcity[setParameter name='env.CGR_ARTIFACTS' value='%s']" % (ReleaseArchive))
+    sys.stdout.flush()
 
     return err
 
 
-def get_cmd_arguments():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(usage="python3 teamcity.py [options]")
     parser.add_argument('--src_hash', default="unknown", help="Sources hash")
     parser.add_argument('--src_dir', default=False, help="Sources directory")
     parser.add_argument('--upload', action='store_true', default=False, help="Upload build")
     parser.add_argument('--clean', action='store_true', default=False, help="Clean build directory")
-    parser.add_argument('--jenkins', action='store_true', default=False, help=argparse.SUPPRESS)
 
-    if sys.platform == 'linux':
-        parser.add_argument('--CGR_CXX_COMPILER', default='/usr/bin/g++4.8', help="Compiler location")
-
-    env_vars = [
-        'BUILD_NUMBER', 'CGR_APPSDK_VERSION', 'CGR_BUILD_MODE', 'CGR_BUILD_TYPE',
-        'CGR_CONFIG_ROOT', 'CGR_HOUDINI_VERSION', 'CGR_HOUDINI_VERSION_BUILD',
-        'CGR_PHXSDK_VERSION', 'CGR_RELEASE_ROOT', 'CGR_SDK', 'CGR_VRAYSDK_VERSION'
-    ]
-
-    default = 'UNSET_DEFAULT'
-
-    for var in env_vars:
-        parser.add_argument('--%s' % var, default=default, help='Optional if not provided will be read from env.')
-
-    args = parser.parse_args()
-
-    stop = False
-
-    for var in env_vars:
-        value = getattr(args, var)
-        if value == default:
-            if var in os.environ:
-                env_val = os.environ[var]
-                sys.stdout.write('Missing value for "%s", reading from environ [%s]\n' % (var, env_val))
-                sys.stdout.flush()
-                setattr(args, var, env_val)
-            else:
-                sys.stderr.write('Missing alue for "%s", which is also missing from env!\n' % var)
-                sys.stderr.flush()
-                stop = True
-
-    return args
-
-
-if __name__ == '__main__':
-    sys.exit(main(get_cmd_arguments()))
+    sys.exit(main(parser.parse_args()))
