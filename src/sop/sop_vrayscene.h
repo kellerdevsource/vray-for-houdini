0b3f784cdada5c980d8755da06e0a8ba88397648
Mila Grigorova <mila.grigorova@chaosgroup.com>
2017-01-17 14:07:31 +0200

    add comments for vrscene sop

--- src/sop/sop_vrayscene.h
+++ src/sop/sop_vrayscene.h
@@ -21,23 +21,46 @@
 namespace VRayForHoudini {
 namespace SOP {
 
+/// SOP that creates geometry from
+/// TODO: this has to be implemented as OBJ network with shop subnet for materials and
+///       custom primitives to show geometry and proper export
 class VRayScene:
 		public SOP::NodeBase
 {
 public:
-	VRayScene(OP_Network *parent, const char *name, OP_Operator *entry):NodeBase(parent, name, entry) {}
-	virtual                    ~VRayScene() {}
+	VRayScene(OP_Network *parent, const char *name, OP_Operator *entry):
+		NodeBase(parent, name, entry)
+	{ }
+	virtual ~VRayScene()
+	{ }
 
-	virtual OP_NodeFlags       &flags() VRAY_OVERRIDE;
-	virtual OP_ERROR            cookMySop(OP_Context &context) VRAY_OVERRIDE;
-	virtual PluginResult        asPluginDesc(Attrs::PluginDesc &pluginDesc, VRayExporter &exporter, ExportContext *parentContext=nullptr) VRAY_OVERRIDE;
+	/// Update flags for this node i.e. if it's time dependent
+	virtual OP_NodeFlags& flags() VRAY_OVERRIDE;
+
+	/// Houdini callback to cook custom geometry for this node
+	/// @param context[in] - cook time
+	virtual OP_ERROR cookMySop(OP_Context &context) VRAY_OVERRIDE;
+
+	/// Callback called by vfh exporter when exporting the node
+	/// @param pluginDesc[out] - dynamic map containing plugin (property, value) pairs
+	/// @param exporter - reference to main vfh exporter
+	/// @param parentContext - context in which this node is exported
+	///                        used to pass some data that will be needed during the export
+	/// @retval result of the export for this node
+	///         PluginResultSuccess - on success
+	///         PluginResultContinue - on success, but singnals that not all relevant properties have been added
+	///         PluginResultError- there has been an error
+	virtual PluginResult asPluginDesc(Attrs::PluginDesc &pluginDesc, VRayExporter &exporter, ExportContext *parentContext=nullptr) VRAY_OVERRIDE;
 
 protected:
-	virtual void                setPluginType() VRAY_OVERRIDE;
+	/// Set custom plugin id and type for this node
+	virtual void setPluginType() VRAY_OVERRIDE;
 
 private:
-	VUtils::Vrscene::Preview::VrsceneSettings            getVrsceneSettings() const;
-	static VUtils::Vrscene::Preview::VrsceneDescManager  m_vrsceneMan;
+	/// Helper function to generate vrscene settings
+	VUtils::Vrscene::Preview::VrsceneSettings getVrsceneSettings() const;
+
+	static VUtils::Vrscene::Preview::VrsceneDescManager  m_vrsceneMan; ///< vrscene manager
 
 }; // VRayScene
 
