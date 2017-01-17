d8b77021ab40c2d68759c7e274b8c90c8cfcbf9b
Mila Grigorova <mila.grigorova@chaosgroup.com>
2017-01-17 13:25:44 +0200

    add comments for proxy primitive

--- src/sop/gu_vrayproxyref.h
+++ src/sop/gu_vrayproxyref.h
@@ -19,15 +19,24 @@
 namespace VRayForHoudini {
 
 
+/// Custom primitive that represents V-Ray proxy file in Houdini
+/// implemented as packed primitive
 class VRayProxyRef:
 		public GU_PackedImpl
 {
 public:
-	/// Get the type ID for the VRayProxy primitive type.
+	/// Get the type ID for the VRayProxy primitive.
 	static GA_PrimitiveTypeId typeId() { return theTypeId; }
+
+	/// Register the primitive with gafactory
+	/// @param gafactory[out] - Houdini primitive factory for custom plugin primitives
 	static void install(GA_PrimitiveFactory *gafactory);
 
 private:
+	/// type id for this primitive
+	/// custom primitive types receive their type id at runtime
+	/// when registering the primitive as opposed to houdini native primitives
+	/// which are known at compile time
 	static GA_PrimitiveTypeId theTypeId;
 
 public:
@@ -35,90 +44,151 @@ public:
 	VRayProxyRef(const VRayProxyRef &src);
 	virtual ~VRayProxyRef();
 
-	/// @{
-	/// Virtual interface from GU_PackedImpl interface
+	/// Get our factory instance
 	virtual GU_PackedFactory* getFactory() const VRAY_OVERRIDE;
-	virtual GU_PackedImpl*    copy() const VRAY_OVERRIDE { return new VRayProxyRef(*this); }
-	virtual bool              isValid() const VRAY_OVERRIDE { return m_detail.isValid(); }
-	virtual void              clearData() VRAY_OVERRIDE;
 
-	virtual bool   load(const UT_Options &options, const GA_LoadMap &) VRAY_OVERRIDE
+	/// Create copy of this primitive
+	virtual GU_PackedImpl* copy() const VRAY_OVERRIDE { return new VRayProxyRef(*this); }
+
+	 /// Test whether the deferred load primitive data is valid
+	virtual bool isValid() const VRAY_OVERRIDE { return m_detail.isValid(); }
+
+	/// The clearData() method is called when the primitives are stashed during
+	/// the cook of a SOP.  See GA_Primitive::stashed().  This gives the
+	/// primitive to optionally clear some data during the stashing process.
+	virtual void clearData() VRAY_OVERRIDE;
+
+	/// Load proxy primitive from options passed as UT_Options
+	/// @param options[in] - options for the proxy primitive
+	/// @param map[in] - load options (for shared primititve data)
+	///                  not used
+	/// @retval true on success
+	virtual bool load(const UT_Options &options, const GA_LoadMap &map) VRAY_OVERRIDE
 	{ return updateFrom(options); }
-	virtual void   update(const UT_Options &options) VRAY_OVERRIDE
+
+	/// Update proxy primitive from options passed as UT_Options
+	/// @param options[in] - options for the proxy primitive
+	virtual void update(const UT_Options &options) VRAY_OVERRIDE
 	{ updateFrom(options); }
-	virtual bool   save(UT_Options &options, const GA_SaveMap &map) const VRAY_OVERRIDE;
 
-//	virtual bool   supportsJSONLoad() const VRAY_OVERRIDE
-//	{ return true; }
-//	virtual bool   loadFromJSON(const UT_JSONValueMap &options, const GA_LoadMap &) VRAY_OVERRIDE
-//	{ updateFrom(options); return true; }
+	/// Save proxy primitive as UT_Options
+	/// @param options[out] - options for the proxy primitive
+	/// @param map[in] - save options (for shared primititve data) - not used
+	/// @retval true on success
+	virtual bool save(UT_Options &options, const GA_SaveMap &map) const VRAY_OVERRIDE;
 
-	virtual bool                   getLocalTransform(UT_Matrix4D &m) const VRAY_OVERRIDE;
-	virtual bool                   getBounds(UT_BoundingBox &box) const VRAY_OVERRIDE;
-	virtual bool                   getRenderingBounds(UT_BoundingBox &box) const VRAY_OVERRIDE;
-	virtual bool                   saveCachedBBox() const VRAY_OVERRIDE { return true; }
-	virtual void                   getVelocityRange(UT_Vector3 &min, UT_Vector3 &max) const VRAY_OVERRIDE;
-	virtual void                   getWidthRange(fpreal &min, fpreal &max) const VRAY_OVERRIDE;
-	virtual bool                   unpack(GU_Detail &destgdp) const VRAY_OVERRIDE;
+	/// Proxy primitive has some options that modify its "intrinsic" transform.
+	/// It is combined with the local transform on the primitive.
+	/// @param m[out] - local trasform for the primitive
+	/// @retval true if local transform is modified
+	virtual bool getLocalTransform(UT_Matrix4D &m) const VRAY_OVERRIDE;
+
+	/// Get bounding box for this primitive (not including transforms)
+	/// @param box[out] - bounds
+	/// @retval true if box has been modified
+	virtual bool getBounds(UT_BoundingBox &box) const VRAY_OVERRIDE;
+
+	/// Get the rendering bounding box for the geometry (not including
+	/// transforms).
+	/// @param box[out] - bounds
+	/// @retval true if box has been modified
+	virtual bool getRenderingBounds(UT_BoundingBox &box) const VRAY_OVERRIDE;
+
+	/// Determines if we should save the bounding box to the metadata, this
+	/// makes sense for fully embedded primitives, but if the primitive
+	/// can be swapped out without the owner knowing, it should be
+	/// avoided.
+	virtual bool saveCachedBBox() const VRAY_OVERRIDE { return true; }
+
+	/// When rendering with velocity blur, the renderer needs to know the
+	/// bounds on velocity to accurately compute the bounding box
+	/// @param min - min velocity
+	/// @param max - max velocity
+	virtual void getVelocityRange(UT_Vector3 &min, UT_Vector3 &max) const VRAY_OVERRIDE;
+
+	/// When rendering points or curves, the renderer needs to know the bounds
+	/// on the width attribute to accurately compute the bounding box.
+	/// @param min - min value
+	/// @param max - max value
+	virtual void getWidthRange(fpreal &min, fpreal &max) const VRAY_OVERRIDE;
+
+	/// Unpack the procedural into a GU_Detail.
+	/// @param destgdp[out] - output geometry
+	/// @retval true on success
+	virtual bool unpack(GU_Detail &destgdp) const VRAY_OVERRIDE;
+
+	/// Get a reference to a const GU_Detail for the packed geometry to display in vewport.
+	/// @param context[in/out] - read/write primitive shared data if necessary
 	virtual GU_ConstDetailHandle   getPackedDetail(GU_PackedContext *context = 0) const VRAY_OVERRIDE;
+
 	/// Report memory usage (includes all shared memory)
-	virtual int64                  getMemoryUsage(bool inclusive) const VRAY_OVERRIDE;
+	/// @param inclusive[in] - if memory used by this primitive (i.e. sizeof(*this))
+	///                        should be included
+	virtual int64 getMemoryUsage(bool inclusive) const VRAY_OVERRIDE;
+
 	/// Count memory usage using a UT_MemoryCounter in order to count
 	/// shared memory correctly.
-	virtual void                   countMemory(UT_MemoryCounter &counter, bool inclusive) const VRAY_OVERRIDE;
-	/// @}
+	/// @param conter[in] - counter for shared memory among all primitives of this type
+	/// @param inclusive[in] - if memory used by this primitive (i.e. sizeof(*this))
+	///                        should be included
+	virtual void countMemory(UT_MemoryCounter &counter, bool inclusive) const VRAY_OVERRIDE;
 
-	/// @{
-	/// Member data accessors for intrinsics
-	const GU_ConstDetailHandle &  getDetail() const { return m_detail; }
-	VRayProxyRef&                 setDetail(const GU_ConstDetailHandle &h) { m_detail = h; return *this; }
+	/// Get/set detail handle for this primitive
+	const GU_ConstDetailHandle& getDetail() const { return m_detail; }
+	VRayProxyRef& setDetail(const GU_ConstDetailHandle &h) { m_detail = h; return *this; }
 
-	inline const UT_Options &            getOptions() const { return m_options.getOptions(); }
-	inline exint                         getGeometryid() const;
+	/// Get relevant proxy plugin params for this primitive
+	inline const UT_Options& getOptions() const { return m_options.getOptions(); }
 
-	inline const char *                  getFilepath() const { return m_options.getFilepath(); }
-	inline void                          setFilepath(const char *filepath);
+	/// Member data accessors for primitive intrinsics
+	///
+	inline exint getGeometryid() const;
 
-	inline exint                         getLOD() const { return m_options.getLOD(); }
-	inline void                          setLOD(exint lod);
-	inline const char *                  getLODName() const;
+	inline const char* getFilepath() const { return m_options.getFilepath(); }
+	inline void setFilepath(const char *filepath);
 
-	inline exint                         getAnimType() const { return m_options.getAnimType(); }
-	inline void                          setAnimType(exint animType);
-	inline const char *                  getAnimTypeName() const;
+	inline exint getLOD() const { return m_options.getLOD(); }
+	inline void setLOD(exint lod);
+	inline const char* getLODName() const;
 
-	inline fpreal64                      getAnimOffset() const { return m_options.getAnimOffset(); }
-	inline void                          setAnimOffset(fpreal64 offset);
+	inline exint getAnimType() const { return m_options.getAnimType(); }
+	inline void setAnimType(exint animType);
+	inline const char* getAnimTypeName() const;
 
-	inline fpreal64                      getAnimSpeed() const { return m_options.getAnimSpeed(); }
-	inline void                          setAnimSpeed(fpreal64 speed);
+	inline fpreal64 getAnimOffset() const { return m_options.getAnimOffset(); }
+	inline void setAnimOffset(fpreal64 offset);
 
-	inline bool                          getAnimOverride() const { return m_options.getAnimOverride(); }
-	inline void                          setAnimOverride(bool override);
+	inline fpreal64 getAnimSpeed() const { return m_options.getAnimSpeed(); }
+	inline void setAnimSpeed(fpreal64 speed);
 
-	inline exint                         getAnimStart() const { return m_options.getAnimStart(); }
-	inline void                          setAnimStart(exint start);
+	inline bool getAnimOverride() const { return m_options.getAnimOverride(); }
+	inline void setAnimOverride(bool override);
 
-	inline exint                         getAnimLength() const { return m_options.getAnimLength(); }
-	inline void                          setAnimLength(exint length);
+	inline exint getAnimStart() const { return m_options.getAnimStart(); }
+	inline void setAnimStart(exint start);
 
-	inline fpreal64                      getScale() const { return m_options.getScale(); }
-	inline void                          setScale(fpreal64 scale);
+	inline exint getAnimLength() const { return m_options.getAnimLength(); }
+	inline void setAnimLength(exint length);
 
-	inline exint                         getFlipAxis() const { return m_options.getFlipAxis(); }
-	inline void                          setFlipAxis(exint flip);
-	/// @}
+	inline fpreal64 getScale() const { return m_options.getScale(); }
+	inline void setScale(fpreal64 scale);
+
+	inline exint getFlipAxis() const { return m_options.getFlipAxis(); }
+	inline void setFlipAxis(exint flip);
 
 private:
-	/// updateFrom() will update from UT_Options only
+	/// Updates primitive options from UT_Options
+	/// and flags detail for update if necessary
 	template <typename T>
 	bool   updateFrom(const T &options);
+
+	/// Clear detail handle
 	void   clearDetail() { m_detail = GU_ConstDetailHandle(); }
 
 private:
-	GU_ConstDetailHandle   m_detail;
-	VRayProxyParms         m_options;
-	int                    m_dirty;
+	GU_ConstDetailHandle   m_detail; ///< detail handle for viewport geometry
+	VRayProxyParms         m_options; ///< holds all relevant proxy plugin params
+	int                    m_dirty; ///< flags if the detail handle needs update
 };
 
 
