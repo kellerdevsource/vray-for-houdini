f015628f782ef29bd59d688d841c4c741ea7bc1f
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-04-07 18:07:55 +0300

    add: pcd setup macro; fix: disable some warnings

--- cmake/vfh_compiler.cmake
+++ cmake/vfh_compiler.cmake
@@ -12,6 +12,35 @@ if(APPLE)
 	set(CMAKE_OSX_DEPLOYMENT_TARGET 10.9)
 endif()
 
+macro(set_precompiled_header PrecompiledHeader PrecompiledSource SourcesVar)
+	if(MSVC)
+		# Remove precompiled from from source list
+		list(REMOVE_ITEM ${SourcesVar} ${PrecompiledSource})
+
+		get_filename_component(PrecompiledBasename ${PrecompiledHeader} NAME_WE)
+
+		set(PrecompiledBinary "${CMAKE_CURRENT_BINARY_DIR}/${PrecompiledBasename}.pch")
+		set(Sources ${${SourcesVar}})
+
+		set_source_files_properties(${PrecompiledSource}
+			PROPERTIES
+				COMPILE_FLAGS "/Yc\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
+				OBJECT_OUTPUTS "${PrecompiledBinary}"
+		)
+
+		set_source_files_properties(${Sources}
+			PROPERTIES
+				COMPILE_FLAGS "/Yu\"${PrecompiledHeader}\" /FI\"${PrecompiledHeader}\" /Fp\"${PrecompiledBinary}\""
+				OBJECT_DEPENDS "${PrecompiledBinary}"
+		)
+
+		# Move PCH source file to the beginning
+		list(INSERT ${SourcesVar} 0 ${PrecompiledSource})
+
+		message(STATUS "Using PCH header: ${PrecompiledHeader}")
+		message(STATUS "Using PCH source: ${PrecompiledSource}")
+	endif()
+endmacro()
 
 function(vfh_osx_flags _project_name)
 	if(APPLE)
@@ -23,12 +52,12 @@ function(vfh_osx_flags _project_name)
 	endif()
 endfunction()
 
-
 if(WIN32)
 	# Houdini specific
 	set(CMAKE_CXX_FLAGS "/wd4355 /w14996 /wd4800 /wd4244 /wd4305 /wd4251 /wd4275 /wd4396 /wd4018 /wd4267 /wd4146 /EHsc /GT /bigobj")
 	# enable multi core compilation
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4838 /wd4805")
 	set(CMAKE_CXX_FLAGS_DEBUG "/Od /MD /Zi /DNDEBUG")
 
 else()
