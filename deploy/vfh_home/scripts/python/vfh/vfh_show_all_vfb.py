07851fa5186cf2d6851dab7e8badeddd2929d96c
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2017-05-05 16:43:54 +0300

    add: VFB and Proxy Import buttons in shelf

---
+++ deploy/vfh_home/scripts/python/vfh/vfh_show_all_vfb.py
@@ -0,0 +1,25 @@
+#
+# Copyright (c) 2015-2016, Chaos Software Ltd
+#
+# V-Ray For Houdini
+#
+# ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+#
+# Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+#
+
+import hou
+import sys
+
+def show_vfbs():
+    didShow = False
+    for node in hou.node('/out/').children():
+        if node.type().name() == 'vray_renderer':
+            for param in node.allParms():
+                if param.name() == 'show_current_vfb':
+                    didShow = True
+                    param.pressButton()
+
+    if not didShow:
+        sys.stdout.write('No vray out nodes found to show VFB for!')
+        sys.stdout.flush()
