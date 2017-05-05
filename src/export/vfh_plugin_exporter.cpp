96c83d6ed33de809c4f1e52da11fb2230073749e
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2017-05-05 18:15:49 +0300

    fix: save and restore render regions

--- src/export/vfh_plugin_exporter.cpp
+++ src/export/vfh_plugin_exporter.cpp
@@ -294,6 +294,13 @@ int VRayPluginRenderer::initRenderer(int hasUI, int reInit)
 			resetCallbacks();
 		}
 		else if (m_vray) {
+			// Workaround to make render regions persist trough renders
+			m_savedRegion.saved = false;
+
+			{
+				m_savedRegion.saved = true;
+				m_vray->getRenderRegion(m_savedRegion.left, m_savedRegion.top, m_savedRegion.width, m_savedRegion.height);
+			}
 			m_vray->stop();
 			m_vray->reset();
 		}
@@ -631,6 +638,12 @@ int VRayPluginRenderer::startRender(int locked)
 {
 	if (m_vray) {
 		Log::getLog().info("Starting render for frame %.3f...", m_vray->getCurrentTime());
+
+		// TODO: unhack this when appsdk saves render regions after m_vray->reset()
+		if (m_savedRegion.saved) {
+			bool succ = m_vray->setRenderRegion(m_savedRegion.left, m_savedRegion.top, m_savedRegion.width, m_savedRegion.height);
+			puts("!");
+		}
 
 		m_vray->start();
 
