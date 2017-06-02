12fb5a1953705894c1e0703249d6a3112e899ce9
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2017-06-02 12:50:30 +0300

    fix: license and ifndef guards added

--- src/vop/brdf/vop_BRDFVRayMtl.h
+++ src/vop/brdf/vop_BRDFVRayMtl.h
@@ -1,4 +1,15 @@
+//
+// Copyright (c) 2015-2016, Chaos Software Ltd
+//
+// V-Ray For Houdini
+//
+// ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+//
+// Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+//
 
+#ifndef VRAY_FOR_HOUDINI_VOP_NODE_BRDF_VRAYMTL_H
+#define VRAY_FOR_HOUDINI_VOP_NODE_BRDF_VRAYMTL_H
 
 #include "vop_node_base.h"
 
@@ -17,3 +28,5 @@ protected:
 
 
 };
+
+#endif
