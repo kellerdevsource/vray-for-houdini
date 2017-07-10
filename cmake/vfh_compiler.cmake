f09d369f840f4a3b565e3001bb8d44a503f4342b
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-07-10 11:09:57 +0300

    fix: flags were overridden

--- cmake/vfh_compiler.cmake
+++ cmake/vfh_compiler.cmake
@@ -56,12 +56,14 @@ endfunction()
 macro(set_compiler_flags)
 	if(WIN32)
 		# Houdini specific
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /EHsc /GT /bigobj")
+		set(CMAKE_CXX_FLAGS "/EHsc /GT /bigobj")
+
 		# Houdini specific warnings
-		set(CMAKE_CXX_FLAGS "/wd4355 /w14996 /wd4800 /wd4244 /wd4305 /wd4275")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4355 /w14996 /wd4800 /wd4244")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4396 /wd4018 /wd4267 /wd4146")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4800 /wd4838 /wd4805 /wd4290")
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4305 /wd4244")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /wd4305 /wd4244 /wd4305 /wd4275")
+
 		# Enable multi core compilation
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
 
