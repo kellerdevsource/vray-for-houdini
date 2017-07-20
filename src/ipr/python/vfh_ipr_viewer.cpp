ac301a7b53cec1727a6548adfd47e636bba7120f
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2017-07-20 14:19:34 +0300

    fix: message queue cleared when resolution changes

--- src/ipr/python/vfh_ipr_viewer.cpp
+++ src/ipr/python/vfh_ipr_viewer.cpp
@@ -238,21 +238,30 @@ public:
 
 	void add(TileQueueMessage *msg) {
 		QMutexLocker locker(&mutex);
-
-		/// Remove previous messages of the same type.
-		QList<int> indexesToRemove;
-		int indexToRemove = 0;
-		for (const TileQueueMessage *queueMsg : queue) {
-			if (queueMsg->type() == msg->type()) {
-				indexesToRemove.append(indexToRemove);
+		
+		if (msg->type() == TileQueueMessage::TileQueueMessageType::messageTypeImageHeader) {
+			/// Clear the queue if new resolution comes
+			for (TileQueueMessage *message : queue) {
+				delete message;
 			}
-			indexToRemove++;
+			queue.clear();
 		}
+		else {
+			/// Remove previous messages of the same type.
+			QList<int> indexesToRemove;
+			int indexToRemove = 0;
+			for (const TileQueueMessage *queueMsg : queue) {
+				if (queueMsg->type() == msg->type()) {
+					indexesToRemove.append(indexToRemove);
+				}
+				indexToRemove++;
+			}
 
-		for (const int index : indexesToRemove) {
-			TileQueueMessage *queueMsg = queue[index];
-			queue.removeAt(index);
-			delete queueMsg;
+			for (const int index : indexesToRemove) {
+				TileQueueMessage *queueMsg = queue[index];
+				queue.removeAt(index);
+				delete queueMsg;
+			}
 		}
 
 		queue.enqueue(msg);
