8e68487c67fdcf27d8d91354d05caf3c84830315
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2017-08-01 13:07:10 +0300

    fix: removed white space alignment, removed virtual keyword

--- src/vop/meta/vop_meta_image_file.h
+++ src/vop/meta/vop_meta_image_file.h
@@ -21,31 +21,31 @@ class MetaImageFile:
 		public NodeBase
 {
 public:
-	static PRM_Template  *GetPrmTemplate();
+	static PRM_Template *GetPrmTemplate();
 
 public:
 	MetaImageFile(OP_Network *parent, const char *name, OP_Operator *entry):NodeBase(parent, name, entry) {}
-	virtual              ~MetaImageFile() {}
+	virtual ~MetaImageFile() {}
 
-	virtual PluginResult  asPluginDesc(Attrs::PluginDesc &pluginDesc, VRayExporter &exporter, ExportContext *parentContext=nullptr) VRAY_OVERRIDE;
+	PluginResult  asPluginDesc(Attrs::PluginDesc &pluginDesc, VRayExporter &exporter, ExportContext *parentContext=nullptr) VRAY_OVERRIDE;
 
-	virtual const char *outputLabel(unsigned idx) const VRAY_OVERRIDE;
-	virtual const char *inputLabel(unsigned idx) const VRAY_OVERRIDE;
+	const char *outputLabel(unsigned idx) const VRAY_OVERRIDE;
+	const char *inputLabel(unsigned idx) const VRAY_OVERRIDE;
 
-	virtual unsigned          getNumVisibleOutputs() const VRAY_OVERRIDE;
-	virtual unsigned          orderedInputs() const VRAY_OVERRIDE;
-	virtual unsigned          maxOutputs() const VRAY_OVERRIDE;
-	virtual unsigned          getNumVisibleInputs() const VRAY_OVERRIDE;
+	unsigned getNumVisibleOutputs() const VRAY_OVERRIDE;
+	unsigned orderedInputs() const VRAY_OVERRIDE;
+	unsigned maxOutputs() const VRAY_OVERRIDE;
+	unsigned getNumVisibleInputs() const VRAY_OVERRIDE;
 protected:
-	virtual void          setPluginType() VRAY_OVERRIDE;
+	void setPluginType() VRAY_OVERRIDE;
 
-	virtual void              getOutputNameSubclass(UT_String &out, int idx) const VRAY_OVERRIDE;
-	virtual void              getInputNameSubclass(UT_String &in, int idx) const VRAY_OVERRIDE;
+	void getOutputNameSubclass(UT_String &out, int idx) const VRAY_OVERRIDE;
+	void getInputNameSubclass(UT_String &in, int idx) const VRAY_OVERRIDE;
 
-	virtual int               getInputFromNameSubclass(const UT_String &in) const VRAY_OVERRIDE;
-	virtual int               getOutputFromName(const UT_String &out) const VRAY_OVERRIDE;
+	int getInputFromNameSubclass(const UT_String &in) const VRAY_OVERRIDE;
+	int getOutputFromName(const UT_String &out) const VRAY_OVERRIDE;
 
-	virtual void              getOutputTypeInfoSubclass(VOP_TypeInfo &type_info, int idx) VRAY_OVERRIDE;
+	void getOutputTypeInfoSubclass(VOP_TypeInfo &type_info, int idx) VRAY_OVERRIDE;
 };
 
 } // namespace VOP
