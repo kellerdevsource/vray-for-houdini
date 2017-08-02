c8aab8b021fd7840b3443c4692f638e24556fe37
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2017-08-02 19:40:50 +0300

    add: messages to asserts

--- src/ipr/python/vfh_process_check_linux.cpp
+++ src/ipr/python/vfh_process_check_linux.cpp
@@ -150,7 +150,7 @@ bool LnxProcessCheck::stop() {
 	if (waitThread) {
 		if (std::this_thread::get_id() == waitThread->get_id()) {
 			Log::getLog().error("LnxProcessCheck::stop() called from waiting thread!");
-			assert(false);
+			assert(false && "LnxProcessCheck::stop() called from waiting thread!");
 		} else if (waitThread->joinable()) {
 			Log::getLog().debug("Calling .join() on proc wait thread");
 			waitThread->join();
@@ -158,7 +158,7 @@ bool LnxProcessCheck::stop() {
 			waitThread = nullptr;
 		} else {
 			Log::getLog().error("Can't join LnxProcessCheck's waiting thread - leaking the handle");
-			assert(false);
+			assert(false && "LnxProcessCheck's wait thread is not joinable");
 			waitThread = nullptr;
 		}
 	}
