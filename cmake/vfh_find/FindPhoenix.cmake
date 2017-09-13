c9c3ee4d823b11576d9c576e8fcbf4cb92c11e40
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-09-13 17:46:36 +0300

    fix: better Phoenix SDK search

--- cmake/vfh_find/FindPhoenix.cmake
+++ cmake/vfh_find/FindPhoenix.cmake
@@ -8,8 +8,9 @@
 # Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
 #
 
+include(vfh_macro)
+
 set(PHXSDK_PATH "" CACHE PATH "Phoenix SDK root location")
-set(PHXSDK_VERSION "" CACHE STRING "Phoenix SDK version")
 
 if(PHXSDK_PATH)
 	# if PHXSDK_PATH is specified then just take it as root location for Phoenix sdk
@@ -46,72 +47,73 @@ else()
 	endif()
 endif()
 
-
 message(STATUS "Phoenix SDK search path: ${_phx_root}")
 
-
-# check if path exists
 if((NOT _phx_root) OR (NOT EXISTS ${_phx_root}))
-
 	set(Phoenix_FOUND FALSE)
-
 else()
-
-	if(WIN32)
-		set(CGR_PHOENIX_SHARED      aurloader.dll)
-		set(CGR_PHOENIX_SHARED_F3D  field3dio_phx.dll)
-		set(CGR_PHOENIX_SHARED_VDB  openvdbio_phx.dll)
-		set(CGR_PHOENIX_STATIC_LIBS guiwin_qt_s.lib
-									iutils_s.lib
-									aurramps_s.lib
-									aurloader_s.lib
-									gui_utils_s.lib)
-	else()
-		set(CGR_PHOENIX_SHARED      libaurloader.so)
-		set(CGR_PHOENIX_SHARED_F3D  libfield3dio_phx.so)
-		set(CGR_PHOENIX_SHARED_VDB  libopenvdbio_phx.so)
-		set(CGR_PHOENIX_STATIC_LIBS libguiwin_qt_s.a
-									libiutils_s.a
-									libaurramps_s.a
-									libaurloader_s.a
-									libgui_utils_s.a)
-	endif()
-
-
-	set(Phoenix_FOUND TRUE)
-	set(Phoenix_INCLUDES "${_phx_root}/include")
+	set(Phoenix_INCLUDES  "${_phx_root}/include")
 	set(Phoenix_LIBRARIES "${_phx_root}/lib")
+	set(Phoenix_LIBS)
 
-	# check for headers
-	foreach(loop_var IN ITEMS   "aurinterface.h"
-								"aurloader.h"
-								"aura_ver.h"
-								"ramps.h")
+	vfh_find_file(VAR CGR_PHOENIX_SHARED
+		NAMES
+			${CMAKE_SHARED_LIBRARY_PREFIX}aurloader${CMAKE_SHARED_LIBRARY_SUFFIX}
+		PATHS
+			${Phoenix_LIBRARIES}
+	)
 
-		if(NOT EXISTS "${Phoenix_INCLUDES}/${loop_var}" )
-			message(STATUS "Invalid Phoenix SDK path - missing file ${Phoenix_INCLUDES}/${loop_var} ")
-			set(Phoenix_FOUND FALSE)
-			break()
+	vfh_find_file(VAR CGR_PHOENIX_SHARED_F3D
+		NAMES
+			${CMAKE_SHARED_LIBRARY_PREFIX}field3dio_phx${CMAKE_SHARED_LIBRARY_SUFFIX}
+		PATHS
+			${Phoenix_LIBRARIES}
+	)
+
+	vfh_find_file(VAR CGR_PHOENIX_SHARED_VDB
+		NAMES
+			${CMAKE_SHARED_LIBRARY_PREFIX}openvdbio_phx${CMAKE_SHARED_LIBRARY_SUFFIX}
+		PATHS
+			${Phoenix_LIBRARIES}
+	)
+
+	set(CGR_PHOENIX_LIB_NAMES
+		aurloader
+		aurloader_s
+		aurramps_s
+		guiwin_qt_s
+		iutils_s
+	)
+
+	foreach(phx_static_lib_name IN ITEMS ${CGR_PHOENIX_LIB_NAMES})
+		vfh_find_library(VAR CGR_PHOENIX_STATIC_LIB_${phx_static_lib_name}
+			NAMES
+				${phx_static_lib_name}
+			PATHS
+				${Phoenix_LIBRARIES}
+		)
+		if(CGR_PHOENIX_STATIC_LIB_${phx_static_lib_name})
+			list(APPEND Phoenix_LIBS ${phx_static_lib_name})
 		endif()
+	endforeach()
 
-	endforeach(loop_var)
+	find_path(CGR_PHOENIX_HEADERS
+		NAMES
+			aura_ver.h
+			aurinterface.h
+			aurloader.h
+			ramps.h
+		PATHS
+			${Phoenix_INCLUDES}
+	)
 
-	# check for libs
-	foreach(loop_var IN ITEMS   "${CGR_PHOENIX_SHARED}"
-								"${CGR_PHOENIX_SHARED_F3D}"
-								"${CGR_PHOENIX_SHARED_VDB}"
-								 ${CGR_PHOENIX_STATIC_LIBS})
-
-		if(NOT EXISTS "${Phoenix_LIBRARIES}/${loop_var}" )
-			message(STATUS "Invalid Phoenix SDK path - missing file ${Phoenix_LIBRARIES}/${loop_var} ")
-			set(Phoenix_FOUND FALSE)
-			break()
-		endif()
-
-	endforeach(loop_var)
-
+	if(CGR_PHOENIX_SHARED AND
+	   CGR_PHOENIX_SHARED_F3D AND
+	   CGR_PHOENIX_SHARED_VDB AND
+	   CGR_PHOENIX_HEADERS)
+		set(Phoenix_FOUND TRUE)
+	endif()
 endif()
-
 
 # check if all paths exist
 if(Phoenix_FOUND)
@@ -129,10 +131,10 @@ if(Phoenix_FOUND)
 	endforeach(loop_var)
 endif()
 
-
 if(NOT Phoenix_FOUND)
 	message(WARNING "Phoenix SDK NOT found!")
 
-	set(Phoenix_INCLUDES Phoenix_INCLUDES-NOTFOUND)
-	set(Phoenix_LIBRARIES Phoenix_LIBRARIES-NOTFOUND)
+	set(Phoenix_INCLUDES  FALSE)
+	set(Phoenix_LIBRARIES FALSE)
+	set(Phoenix_LIBS FALSE)
 endif()
