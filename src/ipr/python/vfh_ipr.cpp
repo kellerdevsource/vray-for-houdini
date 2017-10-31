63f27532c4161cae37f6fe24546c41c2f2e32c39
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2017-10-31 14:55:40 +0200

    fix: code refactor

--- src/ipr/python/vfh_ipr.cpp
+++ src/ipr/python/vfh_ipr.cpp
@@ -287,22 +287,18 @@ static void fillViewParams(VRayExporter &exporter, PyObject *viewParamsDict, Vie
 			cameraNode = CAST_OBJNODE(getOpNodeFromPath(cameraPath));
 		}
 	}
+
+	if (cameraNode) {
+		exporter.fillViewParamsFromCameraNode(*cameraNode, viewParams);
+	}
+
+	fillViewParamsFromDict(viewParamsDict, viewParams);
+	fillRenderRegionFromDict(viewParamsDict, viewParams);
+	
 	if (strcmp("/obj/ipr_camera", camera)) {
-		fillViewParamsFromDict(viewParamsDict, viewParams);
-		fillRenderRegionFromDict(viewParamsDict, viewParams);
-
-		if (cameraNode) {
-			exporter.fillViewParamsFromCameraNode(*cameraNode, viewParams);
-		}
+		exporter.fillViewParamsResFromCameraNode(*cameraNode, viewParams);
 	}
-	else {
-		if (cameraNode) {
-			exporter.fillViewParamsFromCameraNode(*cameraNode, viewParams);
-		}
 
-		fillViewParamsFromDict(viewParamsDict, viewParams);
-		fillRenderRegionFromDict(viewParamsDict, viewParams);
-	}
 	if (cameraNode) {
 		exporter.fillPhysicalViewParamsFromCameraNode(*cameraNode, viewParams);
 	}
