6c95d10c1adac06ffa8bea5dc8bb35773e61cff5
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2017-11-01 15:17:24 +0200

    fix: Dome Light default transformation

--- src/export/export/vfh_export_op.cpp
+++ src/export/export/vfh_export_op.cpp
@@ -118,6 +118,7 @@ void VRayExporter::fillDefaultMappingDesc(DefaultMappingType mappingType, Attrs:
 		case defaultMappingSpherical: {
 			VRay::Matrix uvwTm(1);
 			VUtils::swap(uvwTm[1], uvwTm[2]);
+			uvwTm[2] = -uvwTm[2];
 
 			uvwgenDesc.pluginID = "UVWGenEnvironment";
 			uvwgenDesc.addAttribute(PluginAttr("mapping_type", "spherical"));
