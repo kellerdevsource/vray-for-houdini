8117e9740ab15e50e1a38d00ae170b1f6365bda5
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-11-13 13:01:18 +0200

    misc: temp arg defines

--- src/vfh_includes.h
+++ src/vfh_includes.h
@@ -17,4 +17,29 @@
 #define HDK_16_0_633 UT_MAJOR_VERSION_INT >= 16 && UT_MINOR_VERSION_INT >= 0
 #define HDK_16_5 UT_MAJOR_VERSION_INT >= 16 && UT_MINOR_VERSION_INT >= 5
 
+#if HDK_16_5
+#define GET_SET_ARG_SEP ,
+#define GET_SET_ARG_PRIM_FWD prim
+
+#define GET_SET_ARG_PRIM_SINGLE const GU_PrimPacked *prim
+#define GET_SET_ARG_PRIM GET_SET_ARG_PRIM_SINGLE,
+
+#define SET_ARG_PRIM_SINGLE GU_PrimPacked *prim
+#define SET_ARG_PRIM SET_ARG_PRIM_SINGLE GET_SET_ARG_SEP
+
+#define GET_PRIM_SINGLE const_cast<GU_PrimPacked*>(getPrim())
+#define GET_PRIM GET_PRIM_SINGLE GET_SET_ARG_SEP
+#else
+#define GET_SET_ARG_SEP
+#define GET_SET_ARG_PRIM_FWD
+
+#define GET_SET_ARG_PRIM_SINGLE
+#define GET_SET_ARG_PRIM
+
+#define SET_ARG_PRIM_SINGLE
+#define SET_ARG_PRIM
+
+#define GET_PRIM
+#endif
+
 #endif // VRAY_FOR_HOUDINI_INCLUDES_H
