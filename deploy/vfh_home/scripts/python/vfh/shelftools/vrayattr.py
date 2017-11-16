7cabbe89fd5bd800a35968330d980360cb684acb
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-11-16 14:52:39 +0200

    fix: set label to From Material; allow all VOP

--- deploy/vfh_home/scripts/python/vfh/shelftools/vrayattr.py
+++ deploy/vfh_home/scripts/python/vfh/shelftools/vrayattr.py
@@ -103,7 +103,7 @@ def _addDisplacementControls(ptg, vrayFolder):
         'tags' : {
             'spare_category': 'vray'
         },
-        'menu_labels' : (["Displacement", "Subdivision", "From SHOP"]),
+        'menu_labels' : (["Displacement", "Subdivision", "From Material"]),
         'default_value' : 0,
         'conditionals' : {
             hou.parmCondType.DisableWhen : "{ vray_displ_use == 0 }",
@@ -132,13 +132,13 @@ def _addDisplacementControls(ptg, vrayFolder):
         },
     }))
 
-    ptg.appendToFolder(vrayFolder, hou.FolderParmTemplate("vray_displ_folder_shopnet", "From SHOP", **{
+    ptg.appendToFolder(vrayFolder, hou.FolderParmTemplate("vray_displ_folder_shopnet", "From Material", **{
         'parm_templates' : ([
-            hou.StringParmTemplate("vray_displ_shoppath", "SHOP", 1, **{
+            hou.StringParmTemplate("vray_displ_shoppath", "Material", 1, **{
                 'string_type' : hou.stringParmType.NodeReference,
                 'tags' : {
                     'spare_category': 'vray',
-                    'opfilter': '!!SHOP!!',
+                    'opfilter': '!!VOP!!',
                     'oprelative': '.',
                 }
             })
