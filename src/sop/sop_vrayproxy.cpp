edd6779e22e509c0d748c3a952d43ad90b3bfcfb
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-11-30 11:26:58 +0200

    fix: prim name typo

--- src/sop/sop_vrayproxy.cpp
+++ src/sop/sop_vrayproxy.cpp
@@ -106,7 +106,7 @@ void SOP::VRayProxy::updatePrimitive(const OP_Context &context)
 		GU_PackedImpl *primImpl = m_primPacked->implementation();
 		if (primImpl) {
 #if HDK_16_5
-			primImpl->update(pack, options);
+			primImpl->update(m_primPacked, m_primOptions);
 #else
 			primImpl->update(m_primOptions);
 #endif
