52a7b43c19898d1414ef418aaa98bff7d289e4f7
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-12-08 18:00:42 +0200

    fix: remove unused

--- src/vop/brdf/vop_brdf_def.h
+++ src/vop/brdf/vop_brdf_def.h
@@ -21,34 +21,43 @@ namespace VOP {
 
 #define BRDF_DEF(PluginID) NODE_BASE_DEF(BRDF, PluginID)
 
+// Shader components; enable if needed at some point.
+#define BRDF_COMPONENTS 0
+#if BRDF_COMPONENTS
 BRDF_DEF(BRDFBlinn)
-BRDF_DEF(BRDFBump)
-BRDF_DEF(BRDFCSV)
-BRDF_DEF(BRDFCarPaint)
-BRDF_DEF(BRDFCookTorrance)
 BRDF_DEF(BRDFDiffuse)
 BRDF_DEF(BRDFDiffuse_forSSS)
 BRDF_DEF(BRDFFlakes)
 BRDF_DEF(BRDFGGX)
 BRDF_DEF(BRDFGlass)
 BRDF_DEF(BRDFGlassGlossy)
+BRDF_DEF(BRDFCookTorrance)
 BRDF_DEF(BRDFHOPS)
 BRDF_DEF(BRDFHair)
 BRDF_DEF(BRDFHair2)
-BRDF_DEF(BRDFHair3)
-BRDF_DEF(BRDFHair4)
-BRDF_DEF(BRDFLight)
 BRDF_DEF(BRDFMirror)
 BRDF_DEF(BRDFMultiBump)
 BRDF_DEF(BRDFPhong)
 BRDF_DEF(BRDFSSS)
 BRDF_DEF(BRDFSSS2)
-BRDF_DEF(BRDFSSS2Complex)
 BRDF_DEF(BRDFSampled)
-BRDF_DEF(BRDFSkinComplex)
 BRDF_DEF(BRDFWard)
-BRDF_DEF(BRDFAlSurface)
+#endif
+
+// Experimental plugin.
+#define BRDF_EXPERIMENTAL 0
+#if BRDF_EXPERIMENTAL
 BRDF_DEF(BRDFAlHair)
+#endif
+
+BRDF_DEF(BRDFAlSurface)
+BRDF_DEF(BRDFBump)
+BRDF_DEF(BRDFCarPaint)
+BRDF_DEF(BRDFHair3)
+BRDF_DEF(BRDFHair4)
+BRDF_DEF(BRDFLight)
+BRDF_DEF(BRDFSkinComplex)
+BRDF_DEF(BRDFSSS2Complex)
 
 } // namespace VOP
 } // namespace VRayForHoudini
