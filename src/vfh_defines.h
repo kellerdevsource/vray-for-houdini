4105fb5a11708faf571e752fc8415f703e7ce2f7
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2017-12-18 13:47:03 +0200

    fix: remove unsed; use templates

--- src/vfh_defines.h
+++ src/vfh_defines.h
@@ -11,6 +11,8 @@
 #ifndef VRAY_FOR_HOUDINI_UTIL_DEFINES_H
 #define VRAY_FOR_HOUDINI_UTIL_DEFINES_H
 
+#include "vfh_vray.h"
+
 // QT defines macro "foreach" to be the same as QT_FOREACH which is highly likely to collide
 // openvdb/util/NodeMask has "foreach" method
 // define this macro to stop QT from defining it
@@ -23,39 +25,33 @@
 
 #define STRINGIZE_NX(A) #A
 #define STRINGIZE(A) STRINGIZE_NX(A)
+
 #define NOT(x) !(x)
-
-#define StrEq(nameA, nameB) (vutils_strcmp(nameA, nameB) == 0)
-
-template<typename T>
-void FreePtr(T* &p) {
-	delete p;
-	p = nullptr;
-}
-
-template<typename T>
-void FreePtrArr(T* &p) {
-	delete [] p;
-	p = nullptr;
-}
-
-template <typename T, int N>
-char (&ArraySizeHelper(T (&array)[N]))[N];
-#define CountOf(array) (sizeof(ArraySizeHelper(array)))
-
-#define IsFloatEq(a, b) VUtils::isZero(a - b, 1e-6f)
 
 #define MemberEq(member) (member == other.member)
 #define MemberFloatEq(member) (IsFloatEq(member, other.member))
 #define MemberNotEq(member) (!(member == other.member))
 
-/// Disables copy-constructor and assignment operator
-#define VfhDisableCopy(cls) \
-private: \
-	cls(const cls&); \
-	cls& operator=(const cls&);
-
 #define FOR_IT(type, itName, var) int itName##Idx = 0; for (type::iterator itName = (var).begin(); itName != (var).end(); ++itName, ++itName##Idx)
 #define FOR_CONST_IT(type, itName, var) int itName##Idx = 0; for (type::const_iterator itName = (var).begin(); itName != (var).end(); ++itName, ++itName##Idx)
+
+template <typename T>
+FORCEINLINE void FreePtr(T* &p) {
+	delete p;
+	p = nullptr;
+}
+
+template <typename T>
+FORCEINLINE void FreePtrArr(T* &p) {
+	delete [] p;
+	p = nullptr;
+}
+
+template <typename A>
+FORCEINLINE bool IsFloatEq(const A&, const A&) { vassert(false); return false; }
+template <>
+FORCEINLINE bool IsFloatEq(const float &a, const float &b) { return VUtils::isZero(a - b, 1e-6f); }
+template <>
+FORCEINLINE bool IsFloatEq(const double &a, const double &b) { return VUtils::isZero(a - b, 1e-6); }
 
 #endif // VRAY_FOR_HOUDINI_UTIL_DEFINES_H
