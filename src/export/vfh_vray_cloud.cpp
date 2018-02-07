6267832448fc9444cee7109e5d41728a5c396305
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-02-07 19:55:57 +0200

    add: cloud access and settings classes

---
+++ src/export/vfh_vray_cloud.cpp
@@ -0,0 +1,219 @@
+//
+// Copyright (c) 2015-2017, Chaos Software Ltd
+//
+// V-Ray For Houdini
+//
+// ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+//
+// Full license text:
+//  https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+//
+
+#include "vfh_vray_cloud.h"
+#include "vfh_log.h"
+#include "vfh_defines.h"
+
+#include <QDir>
+#include <QFileInfo>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QProcess>
+#include <QRegExp>
+
+using namespace VRayForHoudini;
+using namespace Cloud;
+
+static const QString keyExecutable = "executable";
+
+static QString vrayCloudClient;
+static int vrayCloudClientChecked = false;
+
+static void findVRayCloudClient()
+{
+	if (vrayCloudClientChecked)
+		return;
+
+	QString cgrHome; 
+#ifdef _WIN32
+	const QProcessEnvironment &env = QProcessEnvironment::systemEnvironment();
+	cgrHome = env.value("APPDATA");
+#else
+	cgrHome = QDir::homePath();
+#endif
+
+	QStringList vcloudDirList;
+	vcloudDirList << cgrHome;
+#ifdef _WIN32
+	vcloudDirList << "Chaos Group";
+#else
+	vcloudDirList << ".ChaosGroup";
+#endif
+	vcloudDirList << "vcloud";
+	vcloudDirList << "client";
+	vcloudDirList << "vcloud.json";
+
+	const QString vcloudFilePath = vcloudDirList.join(QDir::separator());
+
+	QFileInfo vcloudFileInfo(vcloudFilePath);
+	if (vcloudFileInfo.exists()) {
+		QFile vcloudFile(vcloudFileInfo.absoluteFilePath());
+		if (vcloudFile.open(QIODevice::ReadOnly|QIODevice::Text)) {
+			QJsonDocument vcloudJson = QJsonDocument::fromJson(vcloudFile.readAll());
+			vcloudFile.close();
+
+			if (vcloudJson.isObject()) {
+				const QJsonObject vcloudObject = vcloudJson.object();
+				if (vcloudObject.contains(keyExecutable)) {
+					vrayCloudClient = vcloudObject[keyExecutable].toString();
+				}
+			}
+		}
+	}
+
+	vrayCloudClientChecked = true;
+}
+
+JobFilePath::JobFilePath()
+{
+	createFilePath();
+}
+
+JobFilePath::~JobFilePath()
+{
+	removeFilePath();
+}
+
+static int deleteFilePath(const QString &filePath)
+{
+	const int removeRes = QFile::remove(filePath);
+	if (!removeRes) {
+		Log::getLog().error("Failed to remove \"%s\"!", _toChar(filePath));
+	}
+	return removeRes;
+}
+
+void JobFilePath::createFilePath()
+{
+	QFileInfo tempDir(QDir::tempPath());
+	if (!tempDir.isDir() || !tempDir.isWritable()) {
+		Log::getLog().error("Temporary location \"%s\" is not writable!",
+							_toChar(tempDir.absolutePath()));
+	}
+	else {
+		QFileInfo tempFile(tempDir.absoluteFilePath(), "vfhVRayCloud.vrscene");
+		if (tempFile.exists()) {
+			deleteFilePath(tempFile.absoluteFilePath());
+		}
+
+		filePath = tempFile.absoluteFilePath();
+	}
+}
+
+void JobFilePath::removeFilePath()
+{
+	if (!isValid())
+		return;
+
+	deleteFilePath(filePath);
+
+	filePath.clear();
+}
+
+Job::Job(const QString &sceneFile)
+	: sceneFile(sceneFile)
+{}
+
+static QRegExp cloudNameFilter("[^a-zA-Z\\d\\s]");
+
+/// Only letters, digits, spaces and _-.,()[] allowed.
+/// NOTE: QString::remove is non-const.
+static QString filterName(QString value)
+{
+	return value.remove(cloudNameFilter);
+}
+
+void Job::setName(const QString &value)
+{
+	name = filterName(value);
+}
+
+void Job::toArguments(const Job &job, QStringList &arguments)
+{
+	arguments << "--project" << job.project;
+	arguments << "--name" << job.getName();
+	arguments << "--sceneFile" << job.sceneFile;
+
+	arguments << "--renderMode" << job.renderMode;
+
+	if (job.exr) {
+		arguments << "--exr";
+	}
+	else if (job.jpg) {
+		arguments << "--jpg";
+	}
+	else if (job.png) {
+		arguments << "--png";
+		if (job.preserveAlpha) {
+			arguments << "--preserveAlpha";
+		}
+	}
+
+	arguments << "--width" << QString::number(job.width);
+	arguments << "--height" << QString::number(job.height);
+
+	if (job.animation) {
+		arguments << "--animation";
+		arguments << "--frameRange" << QString::number(job.frameRange.first) << QString::number(job.frameRange.second);
+		arguments << "--frameStep" << QString::number(job.frameStep);
+	}
+
+	if (job.ignoreWarnings) {
+		arguments << "--ignoreWarnings";
+	}
+	if (job.noProgress) {
+		arguments << "--no-progress";
+	}
+	if (job.vr) {
+		arguments << "--vr";
+	}
+	if (!job.colorCorrectionsFile.isEmpty()) {
+		arguments << "--colorCorrectionsFile" << job.colorCorrectionsFile;
+	}
+}
+
+static void executeVRayCloudClient(const QStringList &args)
+{
+	Log::getLog().info("Calling V-Ray Cloud Client: %s", _toChar(args.join(" ")));
+
+	QProcess vrayCloudClientProc;
+	vrayCloudClientProc.setProcessChannelMode(QProcess::ForwardedChannels);
+	vrayCloudClientProc.start(vrayCloudClient, args, QIODevice::ReadOnly);
+	vrayCloudClientProc.waitForFinished();
+}
+
+int VRayForHoudini::Cloud::submitJob(const Job &job)
+{
+	findVRayCloudClient();
+
+	if (vrayCloudClient.isEmpty())
+		return false;
+
+	Log::getLog().info("Using V-Ray Cloud Client: \"%s\"", _toChar(vrayCloudClient));
+
+	{
+		QStringList projectArgs;
+		projectArgs << "project" << "create" << "--name" << job.project;
+
+		executeVRayCloudClient(projectArgs);
+	}
+	{
+		QStringList submitArgs;
+		submitArgs << "job" << "submit";
+
+		Job::toArguments(job, submitArgs);
+
+		executeVRayCloudClient(submitArgs);
+	}
+
+	return true;
+}
---
+++ src/export/vfh_vray_cloud.h
@@ -0,0 +1,138 @@
+//
+// Copyright (c) 2015-2017, Chaos Software Ltd
+//
+// V-Ray For Houdini
+//
+// ACCESSIBLE SOURCE CODE WITHOUT DISTRIBUTION OF MODIFICATION LICENSE
+//
+// Full license text:
+//  https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
+//
+
+#ifndef VRAY_FOR_HOUDINI_VFH_VRAY_CLOUD_H
+#define VRAY_FOR_HOUDINI_VFH_VRAY_CLOUD_H
+
+#include "vfh_vray.h"
+
+#include <QStringList>
+#include <QPair>
+
+namespace VRayForHoudini {
+
+class JobFilePath
+{
+public:
+	/// Creates new file path at temporal writable location.
+	JobFilePath();
+
+	/// Removes created file path if exists.
+	~JobFilePath();
+
+	/// Checks if file path is valid.
+	bool isValid() const { return !filePath.isEmpty(); }
+
+	/// Returns file path.
+	QString getFilePath() const { return filePath; }
+
+private:
+	/// Creates new file path.
+	void createFilePath();
+
+	/// Remove file path.
+	void removeFilePath();
+
+	/// Temporary file path for export.
+	QString filePath;
+};
+
+namespace Cloud {
+
+struct Job {
+	typedef QPair<int, int> FrameRange;
+
+	explicit Job(const QString &sceneFile);
+
+	void setName(const QString &value);
+
+	QString getName() const { return name; }
+
+	/// Name of the rendering project.
+	QString project;
+
+	/// Name of the rendering job.
+	QString name;
+
+	/// Path to a .vrscene file (required).
+	const QString sceneFile;
+
+	/// Render Mode (bucket, progressive).
+	QString renderMode = "bucket";
+
+	/// Width of the rendered image.
+	int width{1920};
+
+	/// Height of the rendered image.
+	int height{1080};
+
+	/// Output EXR image.
+	int exr{true};
+
+	/// Output JPG image.
+	int jpg{false};
+
+	/// Output PNG image.
+	int png{false};
+
+	/// Preserve alpha chanel of the PNG output.
+	int preserveAlpha{true};
+
+	/// Render animation instead of a single shot.
+	int animation{false};
+
+	/// Frame Range of the animation.
+	FrameRange frameRange{0, 0};
+
+	/// Frame Step of the animation.
+	int frameStep{1};
+
+	/// Render VR-enabled image.
+	int vr{false};
+
+	/// Ignore warnings in scene file processing.
+	int ignoreWarnings{true};
+
+	/// Do not display progress while submitting a job.
+	int noProgress{false};
+
+	/// Path to a color corrections file.
+	QString colorCorrectionsFile;
+
+	/// Returns RenderMode as string argument value.
+	/// @param renderMode Render mode.
+	static QString renderModeToString(const VRay::RendererOptions::RenderMode renderMode) {
+		switch (renderMode) {
+			case VRay::RendererOptions::RENDER_MODE_PRODUCTION: return "-1";
+			case VRay::RendererOptions::RENDER_MODE_RT_CPU: return "0";
+			case VRay::RendererOptions::RENDER_MODE_RT_GPU_OPENCL: return "1";
+			case VRay::RendererOptions::RENDER_MODE_RT_GPU_CUDA: return "4";
+			case VRay::RendererOptions::RENDER_MODE_PRODUCTION_OPENCL: return "101";
+			case VRay::RendererOptions::RENDER_MODE_PRODUCTION_CUDA: return "104";
+			default:
+				return "-1";
+		}
+	}
+
+	/// Converts settings values to command line arguments.
+	/// @param job Job settings.
+	/// @param[out] arguments Command line arguments list.
+	static void toArguments(const Job &job, QStringList &arguments);
+};
+
+/// Submits job to the V-Ray Cloud using command line V-Ray Cloud Client.
+/// @param job Job settings.
+int submitJob(const Job &job);
+
+} // namespace Cloud
+} // namespace VRayForHoudini
+
+#endif // VRAY_FOR_HOUDINI_VFH_VRAY_CLOUD_H
