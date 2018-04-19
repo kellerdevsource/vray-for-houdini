124663fa34f93be1066f582599625a1f6777ff31
Svetoslav Valkov <svetoslav.valkov@chaosgroup.com>
2018-04-19 15:51:59 +0300

    fix: Newly created objects now shown in RT sessions

--- src/export/vfh_exporter.cpp
+++ src/export/vfh_exporter.cpp
@@ -1931,6 +1931,11 @@ void VRayExporter::exportScene()
 		exportView();
 	}
 
+	if (sessionType == VfhSessionType::rt) {
+		// Add callback to OP Director so new nodes can be exported during RT Sessions
+		OPgetDirector()->addOpInterest(this, RtCallbackOPDirector);
+	}
+
 	bundleMap.init();
 
 	// Clear plugin caches.
