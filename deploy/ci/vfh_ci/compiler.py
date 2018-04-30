af01a4f885151c2d9332eba7867fa5d5bfa1f1f0
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-04-30 15:42:40 +0300

    fix: setup 2017 compiler

--- deploy/ci/vfh_ci/compiler.py
+++ deploy/ci/vfh_ci/compiler.py
@@ -7,7 +7,6 @@
 #
 # Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
 #
-#
 # Setup environment variables for Visual Studio usage from command line.
 #
 
@@ -16,81 +15,48 @@ import sys
 
 from . import utils
 
-
 def setup_ninja():
-    if sys.platform in {'win32'}:
+    if utils.getPlatform() == 'windows':
         ninjaPath = os.path.join(os.environ['VRAY_CGREPO_PATH'], "build_scripts/cmake/tools/bin")
     else:
         ninjaPath = os.path.join(os.environ['CI_ROOT'], "ninja/ninja")
 
     os.environ['PATH'] = os.pathsep.join([ninjaPath] + os.environ['PATH'].split(os.pathsep))
 
-
-def setup_msvc_2012(sdkPath):
-    env = {
-        'INCLUDE' : [
-            "{KDRIVE}/msvs2012/PlatformSDK/Include/shared",
-            "{KDRIVE}/msvs2012/PlatformSDK/Include/um",
-            "{KDRIVE}/msvs2012/PlatformSDK/Include/winrt",
-            "{KDRIVE}/msvs2012/include",
-            "{KDRIVE}/msvs2012/atlmfc/include",
-        ],
-
-        'LIB' : [
-            "{KDRIVE}/msvs2012/PlatformSDK/Lib/win8/um/x64",
-            "{KDRIVE}/msvs2012/atlmfc/lib/amd64",
-            "{KDRIVE}/msvs2012/lib/amd64",
-        ],
-
-        'PATH' : [
-            "{KDRIVE}/msvs2012/bin/amd64",
-            "{KDRIVE}/msvs2012/bin",
-            "{KDRIVE}/msvs2012/PlatformSDK/bin/x64",
-        ] + os.environ['PATH'].split(os.pathsep),
-    }
-
-    for var in env:
-        os.environ[var] = os.pathsep.join(env[var]).format(KDRIVE=sdkPath)
-
-
-def setup_msvc_2015(sdkPath):
+def setup_msvc_2017(sdkPath):
     env = {
         'INCLUDE' : [
             "{KDRIVE}/msvs2015/PlatformSDK/Include/shared",
             "{KDRIVE}/msvs2015/PlatformSDK/Include/um",
             "{KDRIVE}/msvs2015/PlatformSDK/Include/winrt",
             "{KDRIVE}/msvs2015/PlatformSDK/Include/ucrt",
-            "{KDRIVE}/msvs2015/include",
-            "{KDRIVE}/msvs2015/atlmfc/include",
+            "{KDRIVE}/msvs2017/include",
+            "{KDRIVE}/msvs2017/atlmfc/include",
         ],
 
         'LIB' : [
             "{KDRIVE}/msvs2015/PlatformSDK/Lib/winv6.3/um/x64",
             "{KDRIVE}/msvs2015/PlatformSDK/Lib/ucrt/x64",
-            "{KDRIVE}/msvs2015/atlmfc/lib/amd64",
-            "{KDRIVE}/msvs2015/lib/amd64",
+            "{KDRIVE}/msvs2017/atlmfc/lib/amd64",
+            "{KDRIVE}/msvs2017/lib/amd64",
         ],
 
         'PATH' : [
-            "{KDRIVE}/msvs2015/bin/amd64",
-            "{KDRIVE}/msvs2015/bin",
+            "{KDRIVE}/msvs2017/bin/Hostx64/x64",
+            "{KDRIVE}/msvs2017/bin",
             "{KDRIVE}/msvs2015/PlatformSDK/bin/x64",
         ] + os.environ['PATH'].split(os.pathsep),
 
         '__MS_VC_INSTALL_PATH' : [
-            "{KDRIVE}/msvs2015"
+            "{KDRIVE}/msvs2017"
         ],
     }
 
     for var in env:
         os.environ[var] = os.pathsep.join(env[var]).format(KDRIVE=sdkPath)
 
-
 def setup_compiler(houdiniMajorVersion, sdkPath):
     setup_ninja()
 
-    if sys.platform in {'win32'}:
-        if houdiniMajorVersion >= 15.5:
-            setup_msvc_2015(sdkPath)
-        else:
-            setup_msvc_2012(sdkPath)
+    if utils.getPlatform() == 'windows':
+        setup_msvc_2017(sdkPath)
