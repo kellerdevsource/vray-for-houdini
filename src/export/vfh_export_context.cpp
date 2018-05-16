c15a4c7fad72cfe790773a395767e1ec73b6e2b8
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-05-16 20:27:40 +0300

    fix: remove unused

--- src/export/vfh_export_context.cpp
+++ src/export/vfh_export_context.cpp
@@ -7,29 +7,3 @@
 //
 // Full license text: https://github.com/ChaosGroup/vray-for-houdini/blob/master/LICENSE
 //
-
-#include "vfh_export_context.h"
-#include "vfh_exporter.h"
-
-using namespace VRayForHoudini;
-
-ExportContext::ExportContext()
-	: m_type(CT_NULL)
-	, m_exporter(nullptr)
-	, m_target(nullptr)
-	, m_parentContext(nullptr)
-{ }
-
-ExportContext::ExportContext(ContextType type, VRayExporter &exporter, OP_Node &node)
-	: m_type(type)
-	, m_exporter(&exporter)
-	, m_target(&node)
-	, m_parentContext(nullptr)
-{}
-
-ExportContext::ExportContext(ContextType type, VRayExporter &exporter, OP_Node &node, ExportContext &parentContext)
-	: m_type(type)
-	, m_exporter(&exporter)
-	, m_target(&node)
-	, m_parentContext(&parentContext)
-{}
--- src/export/vfh_export_context.h
+++ src/export/vfh_export_context.h
@@ -11,42 +11,9 @@
 #ifndef VRAY_FOR_HOUDINI_EXPORT_CONTEXT_H
 #define VRAY_FOR_HOUDINI_EXPORT_CONTEXT_H
 
-#include <OP/OP_Node.h>
-
 namespace VRayForHoudini {
 
-class VRayExporter;
-
-enum ContextType {
-	CT_NULL = 0,
-	CT_OBJ,
-	CT_SOP,
-	CT_SHOP,
-	CT_VOP,
-	CT_MAX
-};
-
-class ExportContext
-{
-public:
-	ExportContext();
-	ExportContext(ContextType type, VRayExporter &exporter, OP_Node &node);
-	ExportContext(ContextType type, VRayExporter &exporter, OP_Node &node, ExportContext &parentContext);
-	virtual ~ExportContext() { }
-
-	virtual ContextType getType() const { return m_type; }
-	virtual bool        hasType(ContextType type) const { return m_type == type; }
-
-	VRayExporter*  getExporter() const { return m_exporter; }
-	OP_Node*       getTarget() const { return m_target; }
-	ExportContext* getParentContext() const { return m_parentContext; }
-
-protected:
-	ContextType    m_type;
-	VRayExporter  *m_exporter;
-	OP_Node       *m_target;
-	ExportContext *m_parentContext;
-};
+class ExportContext {};
 
 } // namespace VRayForHoudini
 
