2ce6e083ac20571fc0b2f1065e5095df25a32510
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2018-07-03 16:43:24 +0300

    misc: formatting

--- test/vfhutils.py
+++ test/vfhutils.py
@@ -3,18 +3,14 @@
 def getPluginName(opNode, prefix="", suffix=""):
     return "%s@%s|%s|%s" % (prefix, opNode.name(), opNode.parent().name(), suffix)
 
-
 def getLightPluginName(objNode):
     return "Light@%s" % (objNode.name())
-
 
 def getCameraPluginName(objNode):
     return "Camera@%s" % (objNode.name())
 
-
 def getNodePluginName(objNode):
     return "Node@%s" % (objNode.name())
-
 
 def findSubNodesByType(opNode, typeName):
     nodeType = opNode.childTypeCategory().nodeTypes()[ typeName ]
